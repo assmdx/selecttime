f0295eaa4a6e26a6d5cdb44ba1afcf9a74c8497a
renzhenchuan <renzhenchuan@internetware.cn>
2018-06-28 11:30:34 +0800

    .

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const util = require('./util')
+const app = express()
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+app.use(express.static('assets'));
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
+
+//默认页面
+app.get('/',function(req,res){
+   res.sendFile( __dirname + "/assets/" + "index.html" );
+})
+
+//抽号
+app.post('/select', function (req, res) {
+  console.log(req.body)
+   let school = req.body.school
+   let company = req.body.company
+   let phoneNum = req.body.phoneNum
+
+   let findIsExist = util.searchCodeByCS({
+     school:school,
+     company:company
+   })
+
+   let code
+   let group
+   let id
+   if(findIsExist.length === 0){
+     code = util.selectCode()
+     group = code % 65 === 0 ? code / 65 : Math.floor(code/ 65 + 1)
+     id = code % 65 === 0 ? 65 : code - (Math.floor(code / 65 )) * 65
+     util.saveUserDataAndCode({
+       code:code,
+       group:group,
+       id:id,
+       school:school,
+       company:company,
+       phoneNum:phoneNum
+     })
+   }
+   else {
+     code = findIsExist[0].code
+     group = findIsExist[0].group
+     id = findIsExist[0].id
+   }
+
+   res.end(JSON.stringify({
+     code:code,
+     group:group,
+     id:id
+   }))
+})
+
+//重置号码箱
+app.post('/reset',function(req,res){
+  util.clearCode()
+  res.end('ok')
+})
+
+//查看抽号情况
+app.get('/count',function(req,res){
+  res.sendFile( __dirname + "/assets/" + "count.html" );
+})
+
+app.get('/countData',function(req,res){
+  res.end(JSON.stringify(util.getUserData()))
+})
+
+//获取控制台页面
+app.get('/manage',function(req,res){
+  res.sendFile( __dirname + "/assets/" + "manage.html" );
+})
+
+var server = app.listen(22020, function () {})
---
+++ util.js
@@ -0,0 +1,34 @@
+const data = require("./data.json")
+
+var exports = module.exports;
+
+
+exports.selectCode = function(){
+  //随机从数组中取一个数字，填入数组
+  let randomCodeIndex =  parseInt(Math.random()*(data.code.length),10);
+  let code = data.code[randomCodeIndex]
+  data.code.splice(randomCodeIndex,1)
+  return code
+}
+exports.searchCodeByCS = function(userX){
+  const result = data.user.filter(useri => useri.company === userX.company && useri.school === userX.school)
+  return result
+}
+exports.saveUserDataAndCode = function(userX){
+  data.user.push(userX)
+}
+
+exports.clearCode = function(){
+  data.user = []
+  data.code = []
+  let i = 1
+  while(i <= data.maxCode){
+    data.code.push(i)
+    i++
+  }
+}
+
+//排序好的抽号情况
+exports.getUserData = function(){
+  return data.user.sort((a,b) => a.code - b.code)
+}
