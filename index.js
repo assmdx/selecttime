3ba370e6b0271c3bace9043e8eda9496fdf308b0
renzhenchuan <renzhenchuan@internetware.cn>
2018-07-04 07:58:47 +0800

    ADD express添加端口多开功能

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
+var process = require('process')
 const bodyParser = require('body-parser')
 const util = require('./util')
 var app = express()
+const port = (function () {
+  if (typeof (process.argv[2]) !== 'undefined') { // 如果输入了端口号，则提取出来
+    if (isNaN(process.argv[2])) { // 如果端口号不为数字，提示格式错误
+      throw 'Please write a correct port number.'
+    } else { // 如果端口号输入正确，将其应用到端口
+      return process.argv[2]
+    }
+  } else { // 如果未输入端口号，则使用下面定义的默认端口
+    return 22020
+  }
+})()
+
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -118,4 +131,6 @@ app.get('/countData', function(req, res) {
 })
 
 
-var server = app.listen(22020, function() {})
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + ' !')
+})
